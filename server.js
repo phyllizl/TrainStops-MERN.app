3e375299d12da86d5bc63ca131e4b60052457752
fahjin <yongfahjin@hotmail.com>
2021-07-26 18:07:07 +0800

    added express.static

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.get("/data", (req, res) => {
     });
 });
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log("Listening on the port", PORT);
 });
