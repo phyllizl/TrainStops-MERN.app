1ca0145aaa4d56fbf194b4d126dc8b0d7b738d2b
plee <phyllizl@gmail.com>
2021-07-26 11:38:03 +0800

    mime

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 //This will be the landing page where you get: (a) MRT lines, which are (b) drop down lists that show all the stations in that line
 
 const Main = () => {
+
+    const [mrtStations, setMrtStations] = useState([]);
+
+    useEffect(() => {
+        fetch("/v1/mrt")
+        .then((res) => res.json())
+        .then((data) => setMrtStations(data));
+        console.log("data", mrtStations);
+    }, [])
 
     return(
         <>
