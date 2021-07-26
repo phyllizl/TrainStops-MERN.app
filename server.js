3aa628f796aafdf4d8326d17a60b6093b9743f05
fahjin <yongfahjin@hotmail.com>
2021-07-26 18:10:57 +0800

    build 10

--- server.js
+++ server.js
@@ -55,10 +55,10 @@ app.get("/data", (req, res) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
