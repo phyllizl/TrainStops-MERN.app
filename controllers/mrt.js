8016d5fcdfe442bf8b71780a9dad811690bfe200
charlenetan <tanwsc@gmail.com>
2021-07-26 21:45:29 +0800

    edited fetch by mrt id

--- controllers/mrt.js
+++ controllers/mrt.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const MRT = require("../models/mrt");
 const seedMrt = require("../models/seedMrt");
+const { Client } = require("@googlemaps/google-maps-services-js");
 
 //Index route => gets all the MRT stations
 router.get("/", (req, res) => {
@@ -28,11 +29,33 @@ router.get("/mrtSeed", (req, res) => {
 //Find by id
 router.get("/:id", (req, res) => {
   const id = req.params.id;
+  const client = new Client({});
   MRT.findById(id, (err, foundMRT) => {
     if (err) {
       res.status(400).json({ error: err.message });
     }
-    res.status(200).json(foundMRT);
+    const mrtLat = parseFloat(foundMRT["Possible Locations"][0]["LATITUDE"]);
+    console.log(mrtLat);
+    const mrtLng = parseFloat(foundMRT["Possible Locations"][0]["LONGITUDE"]);
+    console.log(mrtLng);
+    client
+      .placesNearby({
+        params: {
+          location: { lat: mrtLat, lng: mrtLng },
+          radius: 500,
+          type: "point_of_interest",
+          key: process.env.APIKEY,
+        },
+        timeout: 1000, // milliseconds
+      })
+      .then((r) => {
+        console.log(r.data.results);
+        res.send(r.data.results);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    // res.status(200).json(foundMRT);
   });
 });
 
