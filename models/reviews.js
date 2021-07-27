d81b95fdde28da0259fa453cb4d674e473c9163a
fahjin <yongfahjin@hotmail.com>
2021-07-27 20:55:14 +0800

    merging to master, useContext not done, upload to own branch first

--- models/reviews.js
+++ models/reviews.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const Users = require("./users");
-const Locations = require("./locations");
 const Schema = mongoose.Schema;
 
 const reviewsSchema = mongoose.Schema({
-  username: { type: Schema.Types.ObjectId, ref: 'Users' },
-  location: [{ type: Schema.Types.ObjectId, ref: 'Locations' }],
+  username: { type: Schema.Types.ObjectId, ref: "Users" },
+  location: { type: String, place_id: "" },
   reviews: { type: String, required: true },
 });
 
