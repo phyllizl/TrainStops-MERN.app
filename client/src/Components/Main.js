eb893e192e02287659540c446dae57ebdb840bec
plee <phyllizl@gmail.com>
2021-07-28 23:45:59 +0800

    dropdown

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -52,22 +52,75 @@ const Main = () => {
     NS.push(CE[0], CE[1]);
   }
 
-  //create handleClick function to track selected mrt
-  const handleClick = (event) => {
-    event.preventDefault();
-    const index = event.target.selectedIndex;
-    const optionElement = event.target.childNodes[index];
-    const optionElementId = optionElement.getAttribute("id");
-    console.log(optionElementId);
-    history.push(`/mrt/${optionElementId}`);
-  };
+  //* CAN DELETE * create handleClick function to track selected mrt
+  // const handleClick = (event) => {
+  //   event.preventDefault();
+  //   const index = event.target.selectedIndex;
+  //   const optionElement = event.target.childNodes[index];
+  //   const optionElementId = optionElement.getAttribute("id");
+  //   console.log(optionElementId);
+  //   history.push(`/mrt/${optionElementId}`);
+  // };
+
+  //Toggle Class for DropDowns
+  const toggle = () => {
+    const EW = document.getElementById("EW");
+    EW.classList.toggle("is-active");
+  }
+
+  const NorthSouth = () => {
+    const NS = document.getElementById("NS");
+    NS.classList.toggle("is-active");
+  }
+
+  const DownTown = () => {
+    const DT = document.getElementById("DT");
+    DT.classList.toggle("is-active");
+  }
+
+  const NorthEast = () => {
+    const NE = document.getElementById("NE");
+    NE.classList.toggle("is-active");
+  }
+
+  const Circle = () => {
+    const CC = document.getElementById("CC");
+    CC.classList.toggle("is-active");
+  }
 
   return (
     <>
+   
     <div class="has-text-centered is-size-4 has-text-weight-bold">
+
+      {/* East West Line */}
+      <div class="py-2 my-2 ">
       <label for="EW"> East West Line </label>
       <br />
 
+      <div class="dropdown" id="EW" onClick={toggle}>
+        <div class="dropdown-trigger">
+          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <span>Choose a Station</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+          <div class="dropdown-content">
+            {EW?.map((ele) => (
+              <a href={`/mrt/${ele?._id}`} class="dropdown-item" key={ele?._id} id={ele?._id}>
+                {ele?.["Station Name"]}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div><br/>
+      </div>
+
+              {/* CAN DELETE
       <select id="EW" onChange={handleClick}>
         <option value="" disabled selected>
           Choose Station
@@ -78,67 +131,112 @@ const Main = () => {
           </option>
         ))}
       </select>
-      <br />
+      <br /> */}
 
+      {/* North South Line */}
+      <div class="py-2 my-2 "> 
       <label for="NS"> North South Line </label>
       <br />
-      <select id="NS" onChange={handleClick}>
-        <option value="" disabled selected>
-          Choose Station
-        </option>
-        {NS?.map((ele) => (
-          <option key={ele?._id} id={ele?._id}>
-            {" "}
-            {ele?.["Station Name"]}{" "}
-          </option>
-        ))}
-      </select>
-      <br />
+      <div class="dropdown" id="NS" onClick={NorthSouth}>
+        <div class="dropdown-trigger">
+          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <span>Choose a Station</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
 
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+          <div class="dropdown-content">
+            {NS?.map((ele) => (
+              <a href={`/mrt/${ele?._id}`} class="dropdown-item" key={ele?._id} id={ele?._id}>
+                {ele?.["Station Name"]}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div><br/>
+      </div>
+
+      {/* DownTown Line */}
+      <div class="py-2 my-2 "> 
       <label for="DT"> Downtown Line </label>
       <br />
-      <select id="DT" onChange={handleClick}>
-        <option value="" disabled selected>
-          Choose Station
-        </option>
-        {DT?.map((ele) => (
-          <option key={ele?._id} id={ele?._id}>
-            {" "}
-            {ele?.["Station Name"]}{" "}
-          </option>
-        ))}
-      </select>
-      <br />
+      <div class="dropdown" id="DT" onClick={DownTown}>
+        <div class="dropdown-trigger">
+          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <span>Choose a Station</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
 
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+          <div class="dropdown-content">
+            {DT?.map((ele) => (
+              <a href={`/mrt/${ele?._id}`} class="dropdown-item" key={ele?._id} id={ele?._id}>
+                {ele?.["Station Name"]}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div><br/>
+      </div>
+
+      {/* Circle Line */}
+      <div class="py-2 my-2 "> 
       <label for="CC"> Circle Line </label>
       <br />
-      <select id="CC" onChange={handleClick}>
-        <option value="" disabled selected>
-          Choose Station
-        </option>
-        {CC?.map((ele) => (
-          <option key={ele?._id} id={ele?._id}>
-            {" "}
-            {ele?.["Station Name"]}{" "}
-          </option>
-        ))}
-      </select>
-      <br />
+      <div class="dropdown" id="CC" onClick={Circle}>
+        <div class="dropdown-trigger">
+          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <span>Choose a Station</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
 
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+          <div class="dropdown-content">
+            {CC?.map((ele) => (
+              <a href={`/mrt/${ele?._id}`} class="dropdown-item" key={ele?._id} id={ele?._id}>
+                {ele?.["Station Name"]}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div><br/>
+      </div>
+      
+      {/* North East Line */}
+      <div class="py-2 my-2 "> 
       <label for="NE"> North East Line </label>
       <br />
-      <select id="NE" onChange={handleClick}>
-        <option value="" disabled selected>
-          Choose Station
-        </option>
-        {NE?.map((ele) => (
-          <option key={ele?._id} id={ele?._id}>
-            {" "}
-            {ele?.["Station Name"]}{" "}
-          </option>
-        ))}
-      </select>
-      <br />
+      <div class="dropdown" id="NE" onClick={NorthEast}>
+        <div class="dropdown-trigger">
+          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <span>Choose a Station</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+          <div class="dropdown-content">
+            {NE?.map((ele) => (
+              <a href={`/mrt/${ele?._id}`} class="dropdown-item" key={ele?._id} id={ele?._id}>
+                {ele?.["Station Name"]}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div><br/>
+      </div>
+     
     </div>
     </>
   );
